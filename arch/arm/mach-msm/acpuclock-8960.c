3a8d64ecd18f15814dab793c0667e864258372b5
gideonx <echen00@gmail.com>
2013-04-02 19:59:04 -0400

    Added lower cpu steppings
    Conflicts:
    	arch/arm/mach-msm/board-8960-regulator.c

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -47,7 +47,7 @@ static struct scalable scalable[] __initdata = {
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x4501,
 		.vreg[VREG_CORE] = { "krait0", 1350000 },
-		.vreg[VREG_MEM]  = { "krait0_mem", 1150000 },
+		.vreg[VREG_MEM]  = { "krait0_mem", 1250000 },
 		.vreg[VREG_DIG]  = { "krait0_dig", 1150000 },
 		.vreg[VREG_HFPLL_A] = { "krait0_s8", 2050000 },
 		.vreg[VREG_HFPLL_B] = { "krait0_l23", 1800000 },
@@ -59,7 +59,7 @@ static struct scalable scalable[] __initdata = {
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x5501,
 		.vreg[VREG_CORE] = { "krait1", 1350000 },
-		.vreg[VREG_MEM]  = { "krait1_mem", 1150000 },
+		.vreg[VREG_MEM]  = { "krait1_mem", 1250000 },
 		.vreg[VREG_DIG]  = { "krait1_dig", 1150000 },
 		.vreg[VREG_HFPLL_A] = { "krait1_s8", 2050000 },
 		.vreg[VREG_HFPLL_B] = { "krait1_l23", 1800000 },
@@ -93,7 +93,7 @@ static struct msm_bus_scale_pdata bus_scale_data __initdata = {
 };
 
 static struct l2_level l2_freq_tbl[] __initdata = {
-	[0]  = { {  384000, PLL_8, 0, 0x00 }, 1050000, 1050000, 1 },
+	[0]  = { {  324000, PLL_8, 0, 0x00 }, 1050000, 1050000, 1 },
 	[1]  = { {  432000, HFPLL, 2, 0x20 }, 1050000, 1050000, 2 },
 	[2]  = { {  486000, HFPLL, 2, 0x24 }, 1050000, 1050000, 2 },
 	[3]  = { {  540000, HFPLL, 2, 0x28 }, 1050000, 1050000, 2 },
@@ -118,7 +118,9 @@ static struct l2_level l2_freq_tbl[] __initdata = {
 #define AVS(x) .avsdscr_setting = (x)
 
 static struct acpu_level acpu_freq_tbl_slow[] __initdata = {
-	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   950000, AVS(0x40001F) },
+	{ 1, {   189000, PLL_8, 0, 0x00 }, L2(0),   950000 },
+        { 1, {   270000, HFPLL, 1, 0x0A }, L2(0),   950000 },
+	{ 1, {   324000, HFPLL, 2, 0x18 }, L2(0),   950000, AVS(0x40001F) },
 	{ 1, {   432000, HFPLL, 2, 0x20 }, L2(6),   975000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),   975000 },
 	{ 1, {   540000, HFPLL, 2, 0x28 }, L2(6),  1000000 },
@@ -159,7 +161,9 @@ static struct acpu_level acpu_freq_tbl_slow[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_nom[] __initdata = {
-	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   900000, AVS(0x40007F) },
+        { 1, {   189000, PLL_8, 0, 0x00 }, L2(0),   900000 },
+        { 1, {   270000, HFPLL, 1, 0x0A }, L2(0),   900000 },
+	{ 1, {   324000, HFPLL, 2, 0x18 }, L2(0),   900000, AVS(0x40007F) },
 	{ 1, {   432000, HFPLL, 2, 0x20 }, L2(6),   925000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),   925000 },
 	{ 1, {   540000, HFPLL, 2, 0x28 }, L2(6),   950000 },
@@ -200,7 +204,9 @@ static struct acpu_level acpu_freq_tbl_nom[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_fast[] __initdata = {
-	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   850000, AVS(0x4000FF) },
+        { 1, {   189000, PLL_8, 0, 0x00 }, L2(0),   850000 },
+        { 1, {   270000, HFPLL, 1, 0x0A }, L2(0),   850000 },
+	{ 1, {   324000, HFPLL, 2, 0x18 }, L2(0),   850000, AVS(0x4000FF) },
 	{ 1, {   432000, HFPLL, 2, 0x20 }, L2(6),   875000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),   875000 },
 	{ 1, {   540000, HFPLL, 2, 0x28 }, L2(6),   900000 },
@@ -243,7 +249,7 @@ static struct acpuclk_krait_params acpuclk_8960_params __initdata = {
 	.l2_freq_tbl_size = sizeof(l2_freq_tbl),
 	.bus_scale = &bus_scale_data,
 	.pte_efuse_phys = 0x007000C0,
-	.stby_khz = 384000,
+	.stby_khz = 324000,
 };
 
 static int __init acpuclk_8960_probe(struct platform_device *pdev)
